9e47cef57edecd7bddd13d73e307bc63ae1bb27b
ashley rae lee <student@epicodus.com>
2014-03-28 17:30:57 -0700

    update spec

--- spec/requesttype_spec.rb
+++ spec/requesttype_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
 describe Requesttype do
-   it { should have_many :financialrequests }
+   it { should have_many :financialrequests } #should have_many_through
    it { should have_many :financialrequest_requesttypes}
 end
